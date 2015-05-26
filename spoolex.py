b6502498a7a8c37d191639c8ae38c124b51037bc
Rodolphe Marques <rodolphe@ascribe.io>
2015-05-26 15:24:17 +0200

    Update comments in spoolex code
    Created tests for the BlochainSpider

--- spoolex.py
+++ spoolex.py
@@ -32,16 +32,31 @@ class InvalidTransactionError(Exception):
 
 
 class BlockchainSpider(object):
+    """
+    Spool blockain explorer. Retrieves from the blockchain the chain of ownership of a hash created
+    with the SPOOL protocol
+    """
 
     def __init__(self, testnet=False):
+        """
+
+        :param testnet: testnet flag. Defaults to False
+        :return: An instance of the BlockainSpider
+        """
         self._t = Transactions(testnet=testnet)
 
     def history(self, hash):
+        """
+        Retrieve the ownership tree of all editions of a piece given the hash
+
+        :param hash: Hash of the file to check. Can be created with the File class
+        :return: ownsership tree of all editions of a piece
+        """
+
         # For now we only support searching the blockchain by the piece hash
         txs = self._t.get(hash)['transactions']
         tree = defaultdict(list)
         number_editions = 0
-        edition_number = 0
 
         for tx in txs:
             _tx = self._t.get(tx['txid'])
@@ -57,7 +72,6 @@ class BlockchainSpider(object):
                 edition_number = verb.edition_number
             else:
                 number_editions = verb.num_editions
-                num_editions_tmp = number_editions
 
             tree[edition_number].append({'txid': txid,
                                          'verb': verb_str,
@@ -77,43 +91,58 @@ class BlockchainSpider(object):
 
     @staticmethod
     def chain(tree, edition_number):
+        """
+
+        :param tree: Tree history of all editions of a piece
+        :param edition_number: The edition number to check for
+        :return: The chain of ownsership of a particular edition of the piece ordered by time
+        """
         # return the chain for an edition_number sorted by the timestamp
         return sorted(tree.get(edition_number, []), key=lambda d: d['timestamp_utc'])
 
     @staticmethod
     def pprint(tree):
+        """
+        Utility function to pretty print the history tree of a piece
+        :param tree: History tree of a piece
+        :return: None
+        """
         p = PrettyPrinter(indent=2)
         p.pprint(tree)
 
     @staticmethod
     def decode_op_return(op_return_hex):
         """
-        Decodes the op_return into a string.
-
-        op_return_hex = 0x6a + length + data
-
-        Args:
-            op_return_hex
-
-        Returns:
-
+        Decodes the op_return hex representation into a string
+        :param op_return_hex: hex representation of the op_return
+        :return: string representation of the op_return
         """
         return binascii.unhexlify(op_return_hex[4:])
 
     @staticmethod
     def check_script(vouts):
+        """
+        Looks into the vouts list of a transaction and returns the op_return if one exists
+        :param vouts: lists of outputs of a transaction
+        :return: string representation of the op_return
+        """
+
         for vout in [v for v in vouts[::-1] if v['extras']['script'].startswith('6a')]:
             verb = BlockchainSpider.decode_op_return(vout['extras']['script'])
-            action = Spoolverb.get_action_from_verb(verb)
+            action = Spoolverb.from_verb(verb).action
             if action in Spoolverb.supported_actions:
                 return verb
         raise Exception("Invalid ascribe transaction")
 
     @staticmethod
     def _get_addresses(tx):
+        """
+        checks for the from, to, and piece address of a SPOOL transactions
+        """
         from_address = set([vin['address'] for vin in tx['vins']])
         if len(from_address) != 1:
-            raise InvalidTransactionError('Transaction should have inputs from only one address {}'.format(from_address))
+            raise InvalidTransactionError("Transaction should have inputs " \
+                                          "from only one address {}".format(from_address))
 
         # order vouts. discard the last vout since its the op_return
         vouts = sorted(tx['vouts'], key=lambda d: d['n'])[:-1]
@@ -125,122 +154,12 @@ class BlockchainSpider(object):
 
     @staticmethod
     def _get_time_utc(time_utc_str):
-        # Receives a string representation of the time in utc
-        # Returns a unix timestamp
+        """
+        Convert a string representation of the time (as returned by blockr.io api) into unix
+        timestamp
+
+        :param time_utc_str: string representation of the time
+        :return: unix timestamp
+        """
         dt = datetime.strptime(time_utc_str, "%Y-%m-%dT%H:%M:%SZ")
         return int(time.mktime(dt.utctimetuple()))
-
-
-
-class SpoolExplorer(object):
-    _EXCLUDE_ADDRESSES = ['1JttRRdtAi6cDNM23Uq4BEU61R8kJeANJs', 'NONSTANDARD']
-
-    def __init__(self, testnet=False, exclude_addresses=[]):
-        self._t = Transactions(testnet=testnet)
-        self._EXCLUDE_ADDRESSES += exclude_addresses
-
-    def history(self, piece_address):
-        """
-
-        Args:
-            address: Address of the piece
-
-        Returns:
-            list with the ownership of the piece in the form [(from_address, spoolverb, to_address), ...]
-
-        """
-        # Check if its a new address.
-        # In the SPOOL protocol a piece is represented has a HD wallet in the bitcoin network
-        # In order to correctly track the provenance of pieces that HD wallet should only perform
-        # operations related to the spool protocol and only for one piece.
-        # In order to register a piece we always need a new address.
-        if not self._t.get(piece_address).get('transactions', []):
-            return []
-
-        piece_txid = self._get_tx_root(piece_address)
-        history = self._follow_transaction(piece_txid, piece_address)
-        return history
-
-    def pprint(self, history):
-        """
-
-        Args:
-            history: list with the ownsership history of the piece
-
-        Returns:
-
-        """
-        for from_address, verb, to_address in history:
-            print "{} -> {} -> {}".format(from_address, verb, to_address)
-
-    def _follow_transaction(self, txid, piece_addr):
-        tx = self._t.get(txid)
-        print '---> ', txid
-
-        if 'transactions' in tx:
-            for _txid in [_tx['txid'] for _tx in tx['transactions'] if _tx['amount'] < 0]:
-                return [] + self._follow_transaction(_txid, piece_addr)
-            return []
-        else:
-            verb = self._check_script(tx['vouts'])
-            from_address = tx['vins'][0]['address']
-            to_address = [vout['address'] for vout in tx['vouts'][::-1] if vout['address'] not in self._EXCLUDE_ADDRESSES + [piece_addr]][0]
-
-            if verb in [spoolverb.register, spoolverb.transfer, spoolverb.consign]:
-                return [(from_address, verb, to_address)] + self._follow_transaction(to_address, piece_addr)
-            elif verb.startswith(spoolverb.loan):
-                date = verb.split(spoolverb.loan)[-1]
-                date = [date[i:i+2] for i in xrange(0, len(date), 2)]
-                date = '-'.join(date[:3]) + ' ' + '-'.join(date[3:])
-                verb = spoolverb.loan + ' ' + date
-                return [(from_address, verb, to_address)] + self._follow_transaction(to_address, piece_addr)
-
-    def _get_tx_root(self, address):
-        """
-        Get the root transaction where the piece with 'hash' was registered
-
-        Args:
-            address: address of the piece to check
-
-        Returns:
-            txid: Transaction id that registered the piece
-
-        """
-        # a piece addr should only have one unspent
-        # TODO: this is no longer true since all transactions have the piece addr. I need to check the first timestamp
-        unspents = self._t.get(address, min_confirmations=1)['unspents']
-        unspents = sorted(unspents, key=lambda d: d['confirmations'], reverse=True)
-        #if len(unspents) != 1:
-        #    raise InvalidTransactionError("Invalid ascribe transaction")
-        txid = unspents[0]['txid']
-
-        # lets get the transaction that created the piece and make sure its a register
-        tx = self._t.get(txid)
-        verb = self._check_script(tx['vouts'])
-        # TODO: Remove the last check. added for debug
-        if verb == spoolverb.register or verb == 'ASCRIBESPOOLREGISTER':
-            return txid
-        else:
-            raise InvalidTransactionError('Invalid ascribe transaction')
-
-    def _decode_op_return(self, op_return_hex):
-        """
-        Decodes the op_return into a string.
-
-        op_return_hex = 0x6a + length + data
-
-        Args:
-            op_return_hex
-
-        Returns:
-
-        """
-        return binascii.unhexlify(op_return_hex[4:])
-
-    def _check_script(self, vouts):
-        for vout in vouts:
-            verb = self._decode_op_return(vout['extras']['script'])
-            # TODO: Remove the last check. added for debug
-            if verb in spoolverb or verb.startswith(spoolverb.loan) or verb.startswith('ASCRIBESPOOLREGISTER'):
-                return verb
-        raise Exception("Invalid ascribe transaction")
---
+++ tests/test_blockchain_spider.py
@@ -0,0 +1,64 @@
+import unittest
+
+from spoolex import BlockchainSpider
+from transactions import Transactions
+
+PIECE_HASH = 'myr2VcDnPKf997sjXx6rUFc4CtFH9sxNVS'
+OP_RETURN_HEX = '6a204153435249424553504f4f4c30314c4f414e312f313530353232313530353233'
+TXID = 'fb22bbb83161f6904f1803ee1cdbed1b5836eb9ac51b102564400989780b48ea'
+
+
+class TestBlockchainSpider(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        bcs = BlockchainSpider(testnet=True)
+        cls.tree = bcs.history(PIECE_HASH)
+
+    def test_history(self):
+        tree = self.tree
+        self.assertEqual(len(tree), 2)
+        self.assertEqual(len(tree[0]), 2)
+        self.assertEqual(len(tree[1]), 5)
+
+    def test_chain(self):
+        tree = self.tree
+
+        chain0 = BlockchainSpider.chain(tree, 0)
+        self.assertEqual(len(chain0), 2)
+
+        chain1 = BlockchainSpider.chain(tree, 1)
+        self.assertEqual(len(chain1), 5)
+
+    def test_decode_op_return(self):
+        decoded_op_return = BlockchainSpider.decode_op_return(OP_RETURN_HEX)
+        self.assertEqual(decoded_op_return, 'ASCRIBESPOOL01LOAN1/150522150523')
+
+    def test_check_scripts(self):
+        t = Transactions(testnet=True)
+        tx = t.get(TXID)
+        vouts = tx['vouts']
+        verb = BlockchainSpider.check_script(vouts)
+        self.assertEqual(verb, 'ASCRIBESPOOL01EDITIONS10')
+
+    def test_data(self):
+        tree = self.tree
+        data = {0: tree[0]}
+        self.assertDictEqual(data, {0: [{'action': 'EDITIONS',
+                                         'edition_number': 0,
+                                         'from_address': u'mqXz83H4LCxjf2ie8hYNsTRByvtfV43Pa7',
+                                         'number_editions': 10,
+                                         'piece_address': u'myr2VcDnPKf997sjXx6rUFc4CtFH9sxNVS',
+                                         'timestamp_utc': 1432646255,
+                                         'to_address': u'n2sQHoUghWUgSM8msqdmCim8pZ635YjoCD',
+                                         'txid': u'fb22bbb83161f6904f1803ee1cdbed1b5836eb9ac51b102564400989780b48ea',
+                                         'verb': 'ASCRIBESPOOL01EDITIONS10'},
+                                       {'action': 'REGISTER',
+                                        'edition_number': 0,
+                                        'from_address': u'mqXz83H4LCxjf2ie8hYNsTRByvtfV43Pa7',
+                                        'number_editions': 10,
+                                        'piece_address': u'myr2VcDnPKf997sjXx6rUFc4CtFH9sxNVS',
+                                        'timestamp_utc': 1432647405,
+                                        'to_address': u'n2sQHoUghWUgSM8msqdmCim8pZ635YjoCD',
+                                        'txid': u'02994a3ceee87be2210fa6e4a649bc0626e791f590bd8db22e7e1fd9fc66d038',
+                                        'verb': 'ASCRIBESPOOL01REGISTER0'}]})
